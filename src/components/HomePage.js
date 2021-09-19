536b8606e5ba8aa6dd0beb12b28b030eeabe239a
Xelus22 <preyas22@gmail.com>
2021-09-19 22:18:29 +1000

    structure

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,7 +11,7 @@ function HomePage(props) {
     const [T1SubsciptionTime, setT1SubsciptionTime] = useState(300);
     const [T2SubsciptionTime, setT2SubsciptionTime] = useState(600);
     const [T3SubsciptionTime, setT3SubsciptionTime] = useState(900);
-    const [socketToken, setSocketToken] = useState("");
+    const [socketToken, setSocketToken] = useState(localStorage.getItem('token'));
     const [color, setColor] = useState("#000000");
     const [fontSize, setFontSize] = useState(150);
     
@@ -19,10 +19,16 @@ function HomePage(props) {
         console.dir(props.hex);
         setColor(props.hex);
     };
+
+    const saveToken = (target) => {
+        setSocketToken(target);
+        localStorage.setItem('token', target);
+    };
     
     return (
         <div style={{ backgroundColor: 'white' }}>
             <h1> Subathon Timer - add this as a browser source to your OBS then interact with it </h1>
+            <h4> Made by <a href = "https://www.twitch.tv/xelus22">Xelus22</a></h4>
             <span> Hours </span>
             <input type="number" id="hours" value={startingHours} onChange={e => setStartingHours(e.target.value)}/>
             <span> Minutes </span>
@@ -48,22 +54,29 @@ function HomePage(props) {
             <br/>
             <span> Streamlabs Socket API Token (MUST BE ENTERED - KEEP SECRET) </span><br/>
             <span> Streamlabs -> settings -> API tokens -> Your Socket API Token </span>
-            <input type="text" id="JWT-Token" value={socketToken} onChange={e => setSocketToken(e.target.value)}/>
-
+            <input type="text" id="JWT-Token" value={socketToken} onChange={e => saveToken(e.target.value)}/>
             <br/>
             <br/>
-
-            <SketchPicker
-                color = { color }
-                onChange={ changeSpy }
-            />
+            <span> Background of timer will be invisible </span><br/>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <SketchPicker
+                                color = { color }
+                                onChange={ changeSpy }
+                            />
+                        </td>
+                        <td><span style={{color: color, fontSize: `${fontSize}px`}}> {("0" + startingHours).slice(-2)}:{("0" + startingMinutes).slice(-2)}:{("0" + startingSeconds).slice(-2)} </span></td>
+                    </tr>
+                </tbody>
+            </table>
             <br/>
             <span> Text size </span>
             <input type="number" value={fontSize} onChange={e => setFontSize(e.target.value)}/>
             <br/>
             <span>  Count down timer size and colour on next page below </span>
             <br/>
-            <span style={{color: color, fontSize: `${fontSize}px`}}> 00:00:00 </span>
             <br/>
             <br/>
             <button>
